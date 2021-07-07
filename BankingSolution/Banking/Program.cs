cb30864aa9c60e6b9a29a9f7bed832c7e12b3ed0
LindaCr <lindacrook.44@gmail.com>
2021-07-06 20:12:45 -0400

    added methods

--- BankingSolution/Banking/Program.cs
+++ BankingSolution/Banking/Program.cs
@@ -8,16 +8,26 @@ namespace Banking
         {
             Console.WriteLine("Hola!");
 
-            var acct1 = new Account()
-            {
-                Id=123, Description="My First Account"
-            };
-            acct1.Deposit(1000);
-            acct1.Withdraw(300);
-            Console.WriteLine($"Balance is {acct1.Balance} ");
+            var sav1 = Savings.OpenSavingsAccount(200);
+            Console.WriteLine($"Id is {sav1.Id}. Balance is {sav1.Balance}");
+            var sav3 = new Savings(300);
+            Console.WriteLine($"Id is {sav3.Id}. Balance is {sav3.Balance}");
+            sav3.Withdraw(50);
+            Console.WriteLine($"Id is  {sav3.Id} . Balance  is {sav3.Balance}");
+            Account.Transfer(50, sav3, sav1);
+            Console.WriteLine($"Id is  {sav3.Id} . Balance is  {sav3 .Balance}");
 
-            acct1.Deposit(-200);
-            Console.WriteLine($"Balance is {acct1.Balance} ");
+            //following code is correct but the method it calls has incomplete exception handling
+           // var acct1 = new Account()
+           // {
+            //    Id=123, Description="My First Account"
+            //};
+            //acct1.Deposit(1000);
+            //acct1.Withdraw(300);
+            //Console.WriteLine($"Balance is {acct1.Balance} ");
+
+            //acct1.Deposit(-200);
+            //Console.WriteLine($"Balance is {acct1.Balance} ");
 
 
 
