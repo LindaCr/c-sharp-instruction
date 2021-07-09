0a671c49a6580873b3d55e6d5b4ca14f9b581270
LindaCr <lindacrook.44@gmail.com>
2021-07-08 21:44:19 -0400

    init commit

---
+++ BowlingCollectionsSolution/BowlingCollections/Bowling.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BowlingCollections
+{
+    class Bowling
+    {
+        Random rand = new Random();
+
+
+        List<int> game = new List<int>();
+
+        //changed method to random to skip entering data one frame at a time
+       // public int Bowl()
+      //  {
+          //  for (var idx= 1; idx<=10; idx++)
+          //  {
+          //      Console.WriteLine($"Enter score for frame {idx}: ");
+          //      var score = Convert.ToInt32(Console.ReadLine());
+          //      game.Add(score);
+          //  }
+         //   var total = 0;
+         //   foreach(var score in game)
+        //    {
+         //       total += score;
+        //    }
+        //    return total;
+       // }
+
+        public int Bowl()
+        {
+            for (var idx = 1; idx <= 10; idx++)
+            {
+                Console.WriteLine($"Enter score for frame {idx}: ");
+                var score = rand.Next(31);
+                game.Add(score);
+            }
+            var total = 0;
+            foreach (var score in game)
+            {
+                total += score;
+            }
+            return total;
+        }
+
+
+
+
+    }
+}
---
+++ BowlingCollectionsSolution/BowlingCollections/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BowlingCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's Go Bowling!");
+
+            var game = new Bowling();
+            Console.WriteLine($"Game score is {game.Bowl()}!");
+
+
+
+
+
+            Console.WriteLine("Peace");
+        }
+    }
+}
