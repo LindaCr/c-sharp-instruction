81b1cec1de7f1b0742460dbc6002e7c3680e394d
LindaCr <lindacrook.44@gmail.com>
2021-07-08 21:46:53 -0400

    init commit

---
+++ ExceptionHandlingSolution/ExceptionHandling/OddNumberException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionHandling
+{
+    public class OddNumberException : Exception
+    {
+        public int OddNumber { get; set; }
+
+
+        //default constructor, must also call Base class (Parent Class) Constructor (added nbr after)
+        public OddNumberException (int Nbr) 
+            : base() {
+            OddNumber = Nbr;
+        }
+
+        //constructor with 1 parameter
+        public OddNumberException(string Message)
+            : base(Message) { }
+
+        //constructor with 2 parameters
+        public OddNumberException(string Message, Exception InnerException)
+            : base(Message, InnerException) { }
+
+    }
+}
---
+++ ExceptionHandlingSolution/ExceptionHandling/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            do
+            {
+                var nbr = Convert.ToInt32(Console.ReadLine());
+                if (nbr % 2 == 1) throw new OddNumberException(nbr);
+                Console.WriteLine("Continue? y/n");
+            } while (Console.ReadLine().ToLower() == "y");
+
+
+
+        }
+    }
+}
