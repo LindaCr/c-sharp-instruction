9c45668a92bf416ea89c24bb5b4921fe03e80c5c
LindaCr <lindacrook.44@gmail.com>
2021-07-13 16:28:35 -0400

    init commit

---
+++ ch7-contact-list/ch7-contact-list/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ch7_contact_list
+{
+    class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+    }
+}
---
+++ ch7-contact-list/ch7-contact-list/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ch7_contact_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Contact List Application!");
+            Console.WriteLine();
+            Contact contact = new Contact();
+
+
+            string choice = "y";
+            while (choice.Equals("y"))
+            {
+                Console.WriteLine("Enter first name: ");
+                var fName = Console.ReadLine();
+                contact.FirstName= fName;
+                Console.WriteLine("Enter last name: ");
+                var lName = Console.ReadLine();
+                contact.LastName = lName;
+                Console.WriteLine("Enter phone number: ");
+                var phone = Console.ReadLine();
+                contact.Phone = phone;
+                Console.WriteLine("Enter email: ");
+                var email = Console.ReadLine();
+                contact.Email = email;
+
+
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine("---------Current Contact--------------");
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine();
+                Console.WriteLine($"Name: {contact.FirstName} {contact.LastName}");
+                Console.WriteLine($"Phone Number: {contact.Phone}");
+                Console.WriteLine($"Email: {contact.Email}");
+
+
+                Console.WriteLine();
+                Console.WriteLine("Continue? y/n");
+                choice = Console.ReadLine().ToLower();
+            }
+
+            Console.WriteLine("Peace");
+        }
+    }
+}
