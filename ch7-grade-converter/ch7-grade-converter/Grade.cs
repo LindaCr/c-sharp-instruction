f200b02cd7620f8c78ea558fefeb76e321781613
LindaCr <lindacrook.44@gmail.com>
2021-07-13 16:29:04 -0400

    init commit

---
+++ ch7-grade-converter/ch7-grade-converter/Grade.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ch7_grade_converter
+{
+    class Grade
+    {
+        public int Number { get; set; }
+        public string Letter { get; set; }
+
+        public Grade()
+        {
+            Number = 0;
+        }
+
+        public Grade(int Number)
+        {
+            this.Number = Number;
+        }
+
+        public string LetterGrade(int Number)
+        {
+            this.Number = Number;
+            if (Number >= 90)
+            {
+                Letter = "A";
+            }
+            else if (Number >= 80)
+            {
+                Letter = "B";
+            }
+            else if (Number >= 70)
+            {
+                Letter = "C";
+            }
+            else if (Number >= 60)
+            {
+                Letter = "D";
+            }
+            else
+            {
+                Letter = "F";
+            }
+            return Letter;
+        }
+
+    }
+}
---
+++ ch7-grade-converter/ch7-grade-converter/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ch7_grade_converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Grade Coverter!");
+            Console.WriteLine();
+            Grade grade = new Grade();
+            string Letter = null;
+
+            var choice = "y";
+            while (choice.Equals("y"))
+            {
+                Console.WriteLine("Enter Grade: ");
+                var nbrGrade = Convert.ToInt32(Console.ReadLine());
+                var letterGrade=grade.LetterGrade(nbrGrade);
+                Console.WriteLine($"Grade is {letterGrade}");
+
+
+                choice = GetRequiredString("Continue? y/n", "y", "n");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Peace");
+
+        }
+
+
+        private static String GetRequiredString(String prompt,
+                                        String s1, String s2)
+        {
+            String str = "";
+            bool isValid = false;
+            while (!isValid)
+            {
+                Console.WriteLine(prompt);
+                str = Console.ReadLine();
+                if (!str.Equals(s1) && !str.Equals(s2))
+                {
+                    Console.WriteLine("Invalid Entry, try again.");
+                }
+                else
+                {
+                    isValid = true;
+                }
+            }
+            return str;
+        }
+    }
+}
