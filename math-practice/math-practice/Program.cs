cf6541e7f0a025e7d0b26ab2aba0854e767d2f02
LindaCr <lindacrook.44@gmail.com>
2021-08-22 21:49:06 -0400

    init commit

---
+++ math-practice/math-practice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace math_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola!");
+
+            var z = 3;
+            var a = 5;
+            var n = 12;
+
+            var sum = z + a + n;
+            var mult = z * a * n;
+            var diff = n - a - z;
+            var div = n / z;
+
+            Console.WriteLine(sum);
+            Console.WriteLine(mult);
+            Console.WriteLine(diff);
+            Console.WriteLine(div);
+
+            Console.WriteLine("Peace");
+        }
+    }
+}
